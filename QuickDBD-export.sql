1070ba51de8086d2a93da412ceb745d5fb8cebd1
taderonde <83925786+taderonde@users.noreply.github.com>
2021-08-07 10:33:41 -0700

    Add files via upload

---
+++ QuickDBD-export.sql
@@ -0,0 +1,40 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+CREATE TABLE "yFinance" (
+    "date" DATETIME   NOT NULL,
+    "open" DECIMAL   NOT NULL,
+    "high" DECIMAL   NOT NULL,
+    "low" DECIMAL   NOT NULL,
+    "close" DECIMAL   NOT NULL,
+    "adj_close" DECIMAL   NOT NULL,
+    "volume" DECIMAL   NOT NULL
+);
+
+CREATE TABLE "finbert" (
+    "id" INT   NOT NULL,
+    "sentence" VARCHAR   NOT NULL,
+    "logit_1" DECIMAL   NOT NULL,
+    "logit_2" DECIMAL   NOT NULL,
+    "logit_3" DECIMAL   NOT NULL,
+    "prediction" VARCHAR   NOT NULL,
+    "sentiment_score" DECIMAL   NOT NULL,
+    CONSTRAINT "pk_finbert" PRIMARY KEY (
+        "id"
+     )
+);
+
+CREATE TABLE "news" (
+    "id" INT   NOT NULL,
+    "title" VARCHAR   NOT NULL,
+    "date" DATETIME   NOT NULL,
+    "stock" VARCHAR   NOT NULL,
+    CONSTRAINT "pk_news" PRIMARY KEY (
+        "id"
+     )
+);
+
+ALTER TABLE "news" ADD CONSTRAINT "fk_news_id" FOREIGN KEY("id")
+REFERENCES "finbert" ("id");
+
